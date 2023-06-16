dc9de94531b320af4b332cdcfc5920defb5899f8
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-06-16 07:52:21 +0000

    [books.controller] 6/15 課題３

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
   get 'about' => 'homes#about'
+  resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
