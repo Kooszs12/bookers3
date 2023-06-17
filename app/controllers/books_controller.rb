edf709763fbb20a4358537b6bc922cacac5f53a1
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-06-17 01:04:59 +0000

    [Stop] 6/17 課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,14 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
-  def cerate
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
   def index
